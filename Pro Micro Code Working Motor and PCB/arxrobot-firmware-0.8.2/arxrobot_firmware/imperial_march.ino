61ef9752d1a00cb495914f573c8b8fa5417d0389
TaeML <taelee7657@yahoo.com>
2016-05-07 21:26:01 -0700

    Added files via upload

---
+++ Pro Micro Code Working Motor and PCB/arxrobot-firmware-0.8.2/arxrobot_firmware/imperial_march.ino
@@ -0,0 +1,14 @@
+void anthem()
+{
+   for (int i = 0; i < length; i++) 
+    {
+      if (notes[i] == "rest") 
+      {
+        delay(beats[i] * tempo);
+      } else {
+        playNote(notes[i], beats[i] * tempo);      
+      }
+      delay(tempo / 2);
+    }
+}
+
